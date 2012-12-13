a9e5d094645665a73a4dc15b997b2f63218c41da
SmoDav <smodavprivate@gmail.com>
2012-12-13 16:57:43 +0300

    initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,103 @@
+# Copyright (C) 2011 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+TARGET_SPECIFIC_HEADER_PATH := device/huawei/u8185/include
+
+# CPU (msm7225a)
+TARGET_ARCH := arm
+TARGET_BOARD_PLATFORM := msm7x27a
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_FPU := neon
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_CORTEX_CACHE_LINE_32 := true
+TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
+
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+# Target properties
+TARGET_BOOTLOADER_BOARD_NAME := u8185
+TARGET_OTA_ASSERT_DEVICE := u8185
+
+# Target information
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_KERNEL := false
+TARGET_NO_RADIOIMAGE := true
+TARGET_NO_RECOVERY := false
+
+# Recovery
+BOARD_LDPI_RECOVERY := true
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/huawei/u8185/recovery/recovery_keys.c
+BOARD_USE_CUSTOM_RECOVERY_FONT := '"font_7x16.h"'
+# force a "go back" menu item
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/huawei/u8815
+TARGET_KERNEL_CONFIG := cyanogenmod_u8185_defconfig
+BOARD_KERNEL_CMDLINE := androidboot.hardware=u8185
+BOARD_KERNEL_BASE := 0x00200000
+BOARD_KERNEL_PAGESIZE := 2048
+
+## OpenGL drivers config file path
+BOARD_EGL_CFG := device/huawei/u8185/config/lib/egl/egl.cfg
+USE_OPENGL_RENDERER := true
+BOARD_USES_QCOM_HARDWARE := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+
+# GPS
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := u8185
+
+# Qcom stuff
+TARGET_USES_OVERLAY := false
+TARGET_HAVE_BYPASS  := false
+TARGET_USES_C2D_COMPOSITION := false
+TARGET_QCOM_HDMI_OUT := false
+TARGET_FORCE_CPU_UPLOAD := true
+TARGET_GRALLOC_USES_ASHMEM := true
+
+# /proc/mtd
+#dev:    size   erasesize  name
+#mtd0: 00500000 00020000 "boot"
+#mtd1: 00500000 00020000 "recovery"
+#mtd2: 00140000 00020000 "misc"
+#mtd3: 00240000 00020000 "MTD-Crash"
+#mtd4: 0ba00000 00020000 "system"
+#mtd5: 03a00000 00020000 "cache"
+#mtd6: 0a500000 00020000 "userdata"
+#mtd7: 01400000 00020000 "userdata2"
+#mtd8: 01400000 00020000 "cust"
+#mtd9: 00140000 00020000 "MIBIB"
+#mtd10: 00060000 00020000 "QCSBL"
+#mtd11: 00800000 00020000 "OEMINFO"
+#mtd12: 001a0000 00020000 "OEMSBL1"
+#mtd13: 001a0000 00020000 "OEMSBL2"
+#mtd14: 01e00000 00020000 "AMSS"
+#mtd15: 00600000 00020000 "EFS2"
+#mtd16: 00060000 00020000 "APPSBL"
+#mtd17: 00040000 00020000 "FOTA"
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00500000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x0ba00000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0a500000
+
+# (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_FLASH_BLOCK_SIZE := 262144 
---
+++ u8185.mk
@@ -0,0 +1,94 @@
+# Copyright (C) 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# init
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/init.u8185.rc:root/init.u8185.rc \
+    $(LOCAL_PATH)/prebuilt/ueventd.u8185.rc:root/ueventd.u8185.rc
+
+# graphics
+PRODUCT_PACKAGES += \
+    libgenlock \
+    gralloc.msm7x27a \
+    copybit.msm7x27a \
+    hwcomposer.msm7x27a
+
+# lights
+PRODUCT_PACKAGES += \
+    lights.msm7x27a
+
+# audio
+PRODUCT_PACKAGES += \
+    audio.primary.msm7x27a \
+    audio_policy.msm7x27a \
+    audio.a2dp.default \
+    libaudioutils
+
+# gps
+PRODUCT_PACKAGES += \
+    gps.u8185
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/config/etc/AudioFilter.csv:system/etc/AudioFilter.csv
+
+# keys
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/usr/keylayout/7k_handset.kl:system/usr/keylayout/7k_handset.kl \
+    $(LOCAL_PATH)/prebuilt/usr/keylayout/u8185_keypad.kl:system/usr/keylayout/u8185_keypad.kl
+
+# hardware permissions
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml
+
+# misc
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/config/etc/media_profiles.xml:system/etc/media_profiles.xml
+
+# dalvik tweaks
+PRODUCT_TAGS += dalvik.gc.type-precise
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=5m \
+    dalvik.vm.heapgrowthlimit=32m \
+    dalvik.vm.heapsize=76m
+
+# Huawei RIL implementation
+# From Dazozzo's decompiled stock GB RIL
+FRAMEWORKS_BASE_SUBDIRS += ../../$(LOCAL_PATH)/ril/
+
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+$(call inherit-product-if-exists, vendor/huawei/u8185/u8185_base.mk)
+
+$(call inherit-product, build/target/product/full_base_telephony.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+
+# LDPI assets
+PRODUCT_AAPT_CONFIG := small ldpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+PRODUCT_LOCALES += ldpi mdpi
+
+PRODUCT_DEVICE := u8185
+PRODUCT_MODEL := U8185
+PRODUCT_BRAND := huawei
+PRODUCT_MANUFACTURER := huawei
